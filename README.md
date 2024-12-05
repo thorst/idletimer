99bb05f2d056f05d99dd7714f65350708f709e57
Todd Horst <toddmhorst@yahoo.com>
2024-12-04 20:21:44 -0500

    1

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # idletimer
-A modern implementation of my old jquery-idletimer library
+A modern implementation of my old [jquery-idletimer library](https://github.com/thorst/jquery-idletimer)
+
+Add reference in your html:
+```html
+<script type="module" src="./idletimer.js"></script>
+```
+
+Here is what your js code will look like:
+```javascript
+import IdleTimer from "./idletimer.js";
+
+// Initialize the idle timer
+const idleTimer = new IdleTimer({
+    timeout: 300000, // 5 minutes
+    onIdle: () => {
+        console.log('User is idle');
+        // Send keep-alive or log out the user, etc.
+        // fetch('/keep-alive');
+    },
+    onActive: () => {
+        console.log('User is active again');
+        // Send any other requests as needed
+    }
+});
+
+// To stop the idle timer (if needed)
+setTimeout(() => {
+    idleTimer.stop();
+    console.log("Idle timer stopped.");
+}, 3600000); // Stop after 1 hour
+
+```
--- idletimer.js
+++ idletimer.js
@@ -1,49 +1,86 @@
 export default class IdleTimer {
-    constructor({ timeout = 60000, onIdle = null, onActive = null } = {}) {
+    constructor({ timeout = 60000, onIdle = null, onActive = null, storageKey = 'lastActive' } = {}) {
         this.timeout = timeout;
         this.onIdle = onIdle;
         this.onActive = onActive;
+        this.storageKey = storageKey;
         this.isIdle = false;
         this.timer = null;
         this.handleEvent = this.handleEvent.bind(this);
+        this.checkTabActivity = this.checkTabActivity.bind(this);
+
+        // Start the timer and event listeners
         this.start();
     }
 
     start() {
         this.isIdle = false;
         this.addActivityListeners();
+        this.addStorageListener();
         this.resetTimer();
     }
 
     stop() {
         clearTimeout(this.timer);
         this.removeActivityListeners();
+        this.removeStorageListener();
     }
 
     resetTimer() {
         clearTimeout(this.timer);
+        this.setLastActiveTime();
         this.timer = setTimeout(() => this.setIdle(), this.timeout);
     }
 
     setIdle() {
         this.isIdle = true;
-        if (typeof this.onIdle === "function") {
-            this.onIdle();
-        }
+        this.setLastActiveTime();
+        this.triggerIdleEvent();
     }
 
     setActive() {
         if (this.isIdle) {
             this.isIdle = false;
-            if (typeof this.onActive === "function") {
-                this.onActive();
-            }
+            this.triggerActiveEvent();
         }
         this.resetTimer();
     }
 
+    setLastActiveTime() {
+        const now = Date.now();
+        localStorage.setItem(this.storageKey, now);
+    }
+
+    checkTabActivity(event) {
+        const lastActive = localStorage.getItem(this.storageKey);
+        const lastActiveTime = lastActive ? parseInt(lastActive, 10) : 0;
+        const inactiveDuration = Date.now() - lastActiveTime;
+
+        // Only send a keep-alive request if all tabs are inactive
+        if (inactiveDuration >= this.timeout) {
+            if (typeof this.onIdle === 'function') {
+                this.onIdle();
+            }
+        } else {
+            // Reset timeout if any tab becomes active
+            this.setActive();
+        }
+    }
+
     handleEvent() {
         this.setActive();
+    }
+
+    triggerIdleEvent() {
+        if (typeof this.onIdle === "function") {
+            this.onIdle();
+        }
+    }
+
+    triggerActiveEvent() {
+        if (typeof this.onActive === "function") {
+            this.onActive();
+        }
     }
 
     addActivityListeners() {
@@ -56,5 +93,13 @@ export default class IdleTimer {
         ["mousemove", "keydown", "scroll", "click", "touchstart"].forEach(event =>
             window.removeEventListener(event, this.handleEvent)
         );
+    }
+
+    addStorageListener() {
+        window.addEventListener('storage', this.checkTabActivity);
+    }
+
+    removeStorageListener() {
+        window.removeEventListener('storage', this.checkTabActivity);
     }
 }
